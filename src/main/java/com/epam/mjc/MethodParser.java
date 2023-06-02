d953d7e2aa53bbe77e64b49f4bdf52e377f05dda
Lapa <lapabig@gmail.com>
2023-06-02 22:21:03 +0600

    Updated

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -2,24 +2,34 @@ package com.epam.mjc;
 
 public class MethodParser {
 
-    /**
-     * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
-     * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
-     * Each argument consists of argument type and argument name, separated by space: ' '.
-     * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
-     *
-     * @param signatureString source string to parse
-     * @return {@link MethodSignature} object filled with parsed values from source string
-     */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        MethodSignature methodSignature = new MethodSignature(null);
+
+        String method = signatureString.substring(0, signatureString.indexOf("("));
+        String arguments = signatureString.substring(signatureString.indexOf("(") + 1, signatureString.indexOf(")")).trim();
+
+        String[] mparts = method.split(" ");
+        String[] aparts = arguments.split(" |, ");
+
+        for (int i = 0; i < mparts.length; i++) {
+            if (mparts[i].equals("public") || mparts[i].equals("private")) {
+                methodSignature.setAccessModifier(mparts[i]);
+            } else if (mparts[i].equals("void") || mparts[i].equals("String") || mparts[i].equals("float")) {
+                methodSignature.setReturnType(mparts[i]);
+            } else {
+                methodSignature.setMethodName(mparts[i]);
+            }
+        }
+        if (aparts.length < 2) {
+            methodSignature.getArguments();
+        } else {
+            for (int i = 0; i < aparts.length; i += 2) {
+                String argumentType = aparts[i].trim();
+                String argumentName = aparts[i + 1].trim();
+                MethodSignature.Argument argument = new MethodSignature.Argument(argumentType, argumentName);
+                methodSignature.getArguments().add(argument);
+            }
+        }
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,22 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class StringSplitter {
 
-    /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
-     *
-     * @param source source string
-     * @param delimiters collection of delimiter strings
-     * @return List of substrings
-     */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> result = new ArrayList<>();
+        String delimiterPattern = String.join("|", delimiters);
+        String[] parts = source.split(delimiterPattern);
+        for (String part : parts) {
+            if (!part.isEmpty()) {
+                result.add(part);
+            }
+        }
+        return result;
     }
 }
+
